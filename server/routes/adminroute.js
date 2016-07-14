c9e94cc842208ee761e5a6568e7418bc27727e7c
Ted Karanikolas <ted@hrg-nyc.com>
2016-07-14 15:15:57 -0500

    full PUT functional

--- server/routes/adminroute.js
+++ server/routes/adminroute.js
@@ -49,21 +49,39 @@ router.delete('/deleteScotch', function (req, res){
 });//end DELETE
 
 router.put('/saveScotch/:id', function(req, res){
-  console.log('Biggles is bringing one down to age', req.body);
-  var id = req.params.id;
-  console.log(id);
+    console.log('Biggles is bringing one down to age', req.body);
+    var entry = req.body;
+    var id = req.params.id;
+    console.log(id);
   pg.connect(connectionString, function(err, client, done){
-    console.log("UPDATE whisky SET region = '" + req.body.region + "' WHERE id  =  " +  id + ";");
-    client.query("UPDATE whisky SET region = '" + req.body.region + "' WHERE id  =  " +  id + ";");
-    if(err){
+    if (err){
+      console.log('Biggles had a connection error.');
       res.sendStatus(500);
-    } else {
-      res.sendStatus(200);
     }
-    done();
-    console.log('Biggles cleaned house.');
-  });
+    client.query('UPDATE whisky ' +
+          'SET region = $1, ' +
+          'distillery = $2, ' +
+          'expression = $3, ' +
+          'palate = $4, ' +
+          'abv = $5, ' +
+          'cask_finish = $6, ' +
+          'whisky_type = $7 ' +
+          'WHERE id = $8',
+     [entry.region, entry.distillery, entry.expression, entry.palate, entry.abv, entry.caks_finish, entry.whisky_type, id],
+     function(err, result){
+       done();
+       if (err){
+         console.log(err);
+         res.sendStatus(500);
+         return;
+       }
+       res.sendStatus(204);
+     });
+   });
 });
 
 module.exports = router;
-//client.query('UPDATE whisky SET region = ' + req.body.region + ' AND WHERE id = ' + id );
+
+// pg.connect(connectionString, function(err, client, done){}
+//   console.log("UPDATE whisky SET region = '" + req.body.region + "' WHERE id  =  " +  id + ";");
+//   client.query("UPDATE whisky SET region = '" + entry.region + "' WHERE id =  " +  id + ";");

58f95c324a6ca1d7ae96960ddc654f7942f31b1f
Ted Karanikolas <ted@hrg-nyc.com>
2016-07-16 20:42:55 -0500

    guestroute needs work

--- server/routes/guestroute.js
+++ server/routes/guestroute.js
@@ -8,10 +8,10 @@ var connectionString = 'postgres://localhost:5432/whiskyDB';
 
 //begin query to whiskyDB
 router.post('/queryOut', function (req, res){
-  console.log('Biggles is heading to the DB bar with his buddies ' + req.body.keyword + ' ' + ' ' + req.body.region + ' ' + ' ' + req.body.scotch_type);
+  console.log('Biggles is heading to the DB bar with his buddies ' + req.body.keyword + ' ' + ' ' + req.body.region_id + ' ' + ' ' + req.body.whisky_type_id);
   var queriedScotch = [];
   pg.connect(connectionString, function(err, client, done){
-    var scotchQuery = client.query( "SELECT * FROM whisky WHERE palate::text ILIKE '%" + req.body.keyword + "%' OR whisky_type = ' + req.body.whisky_type + ' OR region = ' + req.body.region'");
+    var scotchQuery = client.query( "SELECT * FROM whisky WHERE palate::text ILIKE '%" + req.body.keyword + "%' OR whisky_type_id = ' + req.body.whisky_type_id + ' OR region_id = ' + req.body.region_id'");
       console.log('Biggles is in! He is in!');
     scotchQuery.on('row', function(row){
       queriedScotch.push(row);
